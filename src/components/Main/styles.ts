7f2d77a7e92d7fed4eb5a15b1b9acca8ff1c1b64
Alex Maques Alves <atanaelleonardo@gmail.com>
2022-01-17 00:52:47 -0300

    Styling Header from Main Component

--- src/components/Main/styles.ts
+++ src/components/Main/styles.ts
@@ -12,7 +12,30 @@ export const Container = styled.div`
    }
 `;
 
-export const Header = styled.div``;
+export const Header = styled.div`
+   z-index: 2;
+   position: sticky;
+   top: 0;
+   background: var(--primary);
+
+   display: flex;
+   align-items: center;
+
+   text-align: left;
+
+   padding: 8px 0 9px 13px;
+   border-bottom: 1px solid var(--outline);
+
+   > button {
+      padding: 8px;
+      border-radius: 50%;
+      outline: 0;
+      cursor: pointer;
+      &:hover {
+         background: var(--twitter-dark-hover);
+      }
+   }
+`;
 
 export const BackIcon = styled.div``;
 
