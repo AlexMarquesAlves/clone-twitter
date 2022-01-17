6410ad456d1284bc09d90f3d2b3977aba843c878
Alex Maques Alves <atanaelleonardo@gmail.com>
2022-01-17 03:27:43 -0300

    Styling SearchWrapper from SideBar Component

--- src/components/SideBar/styles.ts
+++ src/components/SideBar/styles.ts
@@ -11,7 +11,15 @@ export const Container = styled.div`
    }
 `;
 
-export const SearchWrapper = styled.div``;
+export const SearchWrapper = styled.div`
+   padding: 10px 24px;
+   width: min(399px, 100%);
+   position: fixed;
+   top: 0;
+   z-index: 2;
+   background: var(--primary);
+   max-height: 57px;
+`;
 
 export const SearchInput = styled.div``;
 
