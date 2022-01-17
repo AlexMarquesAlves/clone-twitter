d7c0285acb0e2b3c6ab3d9ff641d5b54bc2479fb
Alex Maques Alves <atanaelleonardo@gmail.com>
2022-01-17 02:09:42 -0300

    Styling Container from Tab Component

--- src/components/Feed/styles.ts
+++ src/components/Feed/styles.ts
@@ -5,4 +5,21 @@ export const Container = styled.div`
    flex-direction: column;
 `;
 
-export const Tab = styled.div``;
+export const Tab = styled.div`
+   margin-top: 10px;
+   padding: 11px 0 15px;
+   text-align: center;
+
+   font-weight: bold;
+   font-size: 15px;
+
+   outline: 0;
+   cursor: pointer;
+
+   color: var(--twitter);
+
+   border-bottom: 2px solid var(--twitter);
+   &:hover {
+      background: var(--twitter-dark-hover);
+   }
+`;
